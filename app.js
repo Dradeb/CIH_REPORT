9c0d798c2cd37736d066109bc498b2634861c892
unknown <youness.dradeb@gmail.com>
2021-09-29 00:19:43 +0100

    few fixes

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+   
+
+});
+
+
 var btn = document.querySelectorAll("nav form .btn")[0];
 var icon = document.querySelectorAll("nav form img")[0];
 var logo = document.querySelectorAll("nav a img")[0];
@@ -60,7 +66,22 @@ function hello(i, filePath) {
                 caro.style.height = "100vh";
                 caro.style.width = "100vw";
                 caro.style.zIndex = "1";
-
+                AOS.init({
+                    // Global settings:
+                    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+                    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+                    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+                    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+                    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+                    delay: 00, // values from 0 to 3000, with step 50ms
+                    duration: 400, // values from 0 to 3000, with step 50ms
+                    easing: 'ease', // default easing for AOS animations
+                    once: false, // whether animation should happen only once - while scrolling down
+                    mirror: true, // whether elements should animate out while scrolling past them
+                    anchorPlacement: 'top-center', // defines which position of the element regarding to window should trigger the animation
+                  
+                  });
+            
             }
         };
         xhttp.open("GET", filePath, true);
