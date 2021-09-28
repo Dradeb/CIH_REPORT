9bccafd8d90fceff667e60b06fba4288bcb35898
unknown <oassade@gmail.com>
2021-09-28 17:30:07 +0100

    overlay multilangue

--- nav/script.js
+++ nav/script.js
@@ -1,9 +1,9 @@
 /* Open when someone clicks on the span element */
 function openNav() {
-    if (document.getElementById("myNav").style.width == "0%") {
-        document.getElementById("myNav").style.width = "100%"
+    if (document.getElementById("myNav").style.left == "-100%") {
+        document.getElementById("myNav").style.left = "0%";
     } else {
-        document.getElementById("myNav").style.width = "0%"
+        document.getElementById("myNav").style.left = "-100%";
     }
 
 }
