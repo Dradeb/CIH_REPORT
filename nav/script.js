39943873361c5aeadf0ab1bd2f50f858390a7459
oassade <oassade@gmail.com>
2021-10-12 16:38:28 +0100

    trash removing

--- nav/script.js
+++ nav/script.js
@@ -4,7 +4,7 @@ function openNav() {
         document.getElementById("myNav").style.left = "0%";
         document.getElementsByClassName('sticky-top')[0].style.background = "#080f29";
         var lang = localStorage.getItem("language") == "ar" ? "ar" : "fr";
-    var path2 = "./images/"+lang+"/Asset 3.png";
+        var path2 = "./images/" + lang + "/Asset 3.png";
         btn.style.color = "white";
         icon.setAttribute("src", "./images/white.png");
         $("#logo").attr("src", path2);
@@ -13,7 +13,5 @@ function openNav() {
         document.getElementById("myNav").style.left = "-100%";
         document.getElementsByClassName('sticky-top')[0].style.background = "#06033600";
     }
-    
-
 }
 
