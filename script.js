170294b6ca6762b15dcf0b30ba516d0f9497daad
oassade <oassade@gmail.com>
2021-10-27 15:58:39 +0100

    modifs

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-var el = "<img src='./images/next.svg' class='animate__animated animate__pulse animate__infinite' height='200px' style='position: relative; top: -75px' />"
-var el2 = "<img src='./images/next.svg' class='animate__animated animate__pulse animate__infinite' height='100px' style='position: relative; top: -55px' />";
+var el = "<div style='position: relative; top: -75px;background-color: white; border-radius: 200px; display:flex; justify-content: center; align-items:center; width:150px; height:150px'><img src='./images/next.svg' class='animate__animated animate__pulse animate__infinite' height='100px' /></div>"
+var el2 = "<div style='position: relative; top: -75px; left: -50px;background-color: white; border-radius: 200px; display:flex; justify-content: center; align-items:center; width:150px; height:150px'><img src='./images/next.svg' class='animate__animated animate__pulse animate__infinite' height='100px' />";
 var next = document.getElementsByClassName("next")[0];
 var prev = document.getElementsByClassName("previous")[0];
 next.innerHTML = el;
 prev.innerHTML = el2;
 prev.style.transform = "rotateY(180deg)";
-prev.style.left = "100px";
