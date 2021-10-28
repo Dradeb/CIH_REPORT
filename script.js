7a5c6e16c0d1fc3c693f390a24ee99972fd92115
oassade <oassade@gmail.com>
2021-10-28 11:44:32 +0100

    mot du president next

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var el = "<div style='position: relative; top: -75px;background-color: white; border-radius: 200px; display:flex; justify-content: center; align-items:center; width:150px; height:150px'><img src='./images/next.svg' class='animate__animated animate__pulse animate__infinite' height='100px' /></div>"
+var el = "<div style='position: relative; top: -50px;background-color: white; border-radius: 200px; display:flex; justify-content: center; align-items:center; width:150px; height:150px'><img src='./images/next.svg' class='animate__animated animate__pulse animate__infinite' height='100px' /></div>"
 var el2 = "<div style='position: relative; top: -75px; left: -50px;background-color: white; border-radius: 200px; display:flex; justify-content: center; align-items:center; width:150px; height:150px'><img src='./images/next.svg' class='animate__animated animate__pulse animate__infinite' height='100px' />";
 var next = document.getElementsByClassName("next")[0];
 var prev = document.getElementsByClassName("previous")[0];
