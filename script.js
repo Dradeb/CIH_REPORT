f6535f2d1419641c09e33cb6a4938b20c30b78e4
oassade <oassade@gmail.com>
2021-11-01 14:51:21 +0100

    resau part changed

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-var el = "<div style='position: relative; top: -50px;background-color: white; border-radius: 200px; display:flex; justify-content: center; align-items:center; width:100px; height:100px'><img src='./images/next.svg' class='animate__animated animate__pulse animate__infinite' height='60%' /></div>"
-var el2 = "<div style='position: relative; top: -75px; left: -50px;background-color: white; border-radius: 200px; display:flex; justify-content: center; align-items:center; width:100px; height:100px'><img src='./images/next.svg' class='animate__animated animate__pulse animate__infinite' height='60%' />";
+var el = "<div style='position: relative;background-color: white; border-radius: 200px; border: 2px dashed #0f548f ; display:flex; justify-content: center; align-items:center; width:100px; height:100px'><img src='./images/next.svg' class='animate__animated animate__pulse animate__infinite' height='60%' /></div>"
+var el2 = "<div style='position: relative;background-color: white; border-radius: 200px;border: 2px dashed #0f548f ; display:flex; justify-content: center; align-items:center; width:100px; height:100px'><img src='./images/next.svg' class='animate__animated animate__pulse animate__infinite' height='60%' />";
 var next = document.getElementsByClassName("next")[0];
 var prev = document.getElementsByClassName("previous")[0];
 next.innerHTML = el;
 prev.innerHTML = el2;
-prev.style.transform = "rotateY(180deg)";
--- script2.js
+++ script2.js
@@ -1,8 +1,4 @@
-var el = "<a href='timeline.html'><img src='./images/next.png' class='animate__animated animate__pulse animate__infinite' height='200px' style='position: relative; top: -75px' ></a>";
-var el2 = "<img src='./images/next.png' class='animate__animated animate__pulse animate__infinite' height='100px' style='position: relative; top: -55px' >";
+var el = "<a href='timeline.html'><div style='position: relative;background-color: white; border-radius: 200px;border: 2px dashed #0f548f ; display:flex; justify-content: center; align-items:center; width:100px; height:100px'><img src='./images/next.svg' class='animate__animated animate__pulse animate__infinite' height='60%' /></div></a>";
 var next = document.getElementsByClassName("next")[0];
 var prev = document.getElementsByClassName("previous")[0];
 next.innerHTML = el;
-prev.innerHTML = el2;
-prev.style.transform = "rotateY(180deg)";
-prev.style.left = "100px";
