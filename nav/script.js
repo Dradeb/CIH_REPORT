f49e851f1832ea5d6a2e4dd676a870d98700d63c
oassade <oassade@gmail.com>
2021-11-22 11:46:46 +0100

    nav fixed in index

--- nav/script.js
+++ nav/script.js
@@ -1,5 +1,5 @@
 /* Open when someone clicks on the span element */
-function openNav() {
+function openNav(url) {
     if (document.getElementById("myNav").style.left != "-100%") {
         document.getElementById("myNav").style.left = "-100%";
         document.getElementsByClassName('sticky-top')[0].style.background = "#06033600";
@@ -7,11 +7,12 @@ function openNav() {
         document.getElementById("myNav").style.left = "0%";
         document.getElementsByClassName('sticky-top')[0].style.background = "#080f29";
         var lang = localStorage.getItem("language") == "ar" ? "ar" : "fr";
-        var path2 = "../images/" + lang + "/Asset 3.png";
+        var link = url ? url : "../images/"
+        var path2 = link + lang + "/Asset 3.png";
         btn.style.color = "white";
-        icon.setAttribute("src", "../images/white.svg");
+        icon.setAttribute("src", link + "../images/"+"white.svg");
         $("#logo").attr("src", path2);
-        bar.setAttribute("src", "../images/bar.png");
+        bar.setAttribute("src", link + "../images/"+"bar.png");
         if (window.innerWidth <= 600) {
             var navs = document.querySelectorAll(".overlay-content .row.d-flex");
             for(var n in navs){
