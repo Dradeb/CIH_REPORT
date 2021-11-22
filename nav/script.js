cd79990789fd31e6e2d6236fe3f3adf318efbdb3
oassade <oassade@gmail.com>
2021-11-22 10:26:38 +0100

    cih 2.0

--- nav/script.js
+++ nav/script.js
@@ -8,11 +8,10 @@ function openNav() {
         document.getElementsByClassName('sticky-top')[0].style.background = "#080f29";
         var lang = localStorage.getItem("language") == "ar" ? "ar" : "fr";
         var path2 = "../images/" + lang + "/Asset 3.png";
-        console.log(path2)
         btn.style.color = "white";
-        icon.setAttribute("src", "../images/" + "white.svg");
+        icon.setAttribute("src", "../images/white.svg");
         $("#logo").attr("src", path2);
-        bar.setAttribute("src", "../images/" + "bar.png");
+        bar.setAttribute("src", "../images/bar.png");
         if (window.innerWidth <= 600) {
             var navs = document.querySelectorAll(".overlay-content .row.d-flex");
             for(var n in navs){
